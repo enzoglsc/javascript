fd87d523a6da0b561ce00512824aea1282b27204
enzoglsc <enzogabthee@gmail.com>
2025-05-21 12:22:52 -0300

    att
    att

--- comparacao.js
+++ comparacao.js
@@ -6,6 +6,9 @@
 // < menor que
 // >= maior ou igual 
 //<= menor ou igual 
+// && e
+// || ou
+// | nao
 
 console.log(5=="5")
 console.log(5==="5")
@@ -28,3 +31,15 @@ let temperatura = 45
 
 console.log("Esta quente?",temperatura >= 30)
 
+// Ex:
+
+console.log(true && false)
+console.log(true || false)
+
+// Ex:
+
+let idade2 = 21
+let temCarteira = true 
+let podeDirigir = idade >= 18 && temCarteira
+
+console.log("Pode dirigir?", podeDirigir)
---
+++ controledefluxo.js
@@ -0,0 +1,12 @@
+// if executa se a condicao for true
+// else if executa se a outra condicao for true
+// else se nao 
+
+// Ex:
+
+let idade = 19
+if (idade >=18) {
+    console.log("Maior de idade")
+} else {
+    console.log("Menor de idade")
+}
