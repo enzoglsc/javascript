9bb5b4a5f5a06e1f6ea510e3487ba777377e4f5d
enzoglsc <enzogabthee@gmail.com>
2025-05-21 11:44:29 -0300

    attatt
    att

--- comparacao.js
+++ comparacao.js
@@ -14,3 +14,17 @@ console.log(5==="5")
 
 let idade = 18 
 console.log(idade>=18)
+
+// Ex:
+
+let SenhaCorreta = "1234"
+let SenhaDigitada = "1234"
+
+console.log(SenhaCorreta===SenhaDigitada)
+
+// Ex:
+
+let temperatura = 45
+
+console.log("Esta quente?",temperatura >= 30)
+
